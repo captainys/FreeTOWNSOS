14cf7b01f2f5ff3ad73f8a1c05a6924f239d5338
Soji Yamakawa <soji@andrew.cmu.edu>
2024-09-07 16:15:22 -0400

    Working on MINVCPI.SYS

--- scripts/run.py
+++ scripts/run.py
@@ -21,9 +21,13 @@ proc=subprocess.Popen([
 	os.path.join(THISDIR,"..","..","TOWNSEMU_TEST","DISKIMG","TBIOSV1.1L30.d77"),
 	"-DEBUG",
 	"-INITCMD","ENA FDCMON",
-	"-INITCMD","BRKON INT 21 AX=4B03", # Stop at driver installation
-	"-SHAREDDIR",os.path.join(THISDIR,"..","tgdrv")
-]+sys.argv[1:])
+	# "-INITCMD","BRKON INT 21 AX=4B03", # Stop at driver installation
+	"-SHAREDDIR",os.path.join(THISDIR,"..","tgdrv"),
+	# "-POWEROFFAT","2B3D:100",
+	# "-UNITTEST",
 
-proc.wait()
-
+	"-initcmd","SAVESTATEAT 50:0 00500000.tstate",
+	"-initcmd","SAVESTATEAT B800:0 B8000000.tstate",
+	"-initcmd","SAVESTATEAT B800:262 B8000262.tstate",
+	"-initcmd","SAVESTATEAT B800:2CC B80002CC.tstate",
+]+sys.argv[1:]).wait()
