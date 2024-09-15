6eca44182bf4cf6bc1e4df5b678486af086a696f
Soji Yamakawa <soji@andrew.cmu.edu>
2024-09-14 23:01:54 -0400

    Start compatible TBIOS

---
+++ tgbios/EGB.C
@@ -0,0 +1,17 @@
+void EGB_ENTRY(
+	unsigned int EDI,
+	unsigned int ESI,
+	unsigned int EBP,
+	unsigned int ESP,
+	unsigned int EBX,
+	unsigned int EDX,
+	unsigned int ECX,
+	unsigned int EAX,
+	unsigned int DS,
+	unsigned int ES,
+	unsigned int GS,
+	unsigned int FS)
+{
+	_Far unsigned char *ptr;
+	
+}
