9a1c0c2fa1e04555d977088e7d44fac4ac5445b0
Soji Yamakawa <soji@andrew.cmu.edu>
2024-10-03 17:19:51 -0400

    Trying to fix Pitch Bend.

--- experiments/maketable.c
+++ experiments/maketable.c
@@ -5,7 +5,7 @@ int main(void)
 	int i;
 	for(i=-8192; i<=8192; i+=64)
 	{
-		double e=(double)i/4096.0;
+		double e=(double)i/8192.0;
 		double s=pow(2.0,e);
 		printf("\t0x%08x, // %lf\n",(int)(s*65536),s);
 	}
