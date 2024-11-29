458b64194637dd2fbe9da34fc4192adf202cb8a7
Soji Yamakawa <soji@andrew.cmu.edu>
2024-11-28 20:20:57 -0500

    RS232C debug in SND_25H.C

--- tests/tgbios/SND_25H.C
+++ tests/tgbios/SND_25H.C
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <snd.h>
 
+#include "PLTDEBUG.H"
+#include "RSDEBUG.H"
+
 char SND_work[SndWorkSize];
 
 
@@ -9,6 +12,9 @@ int main(int ac,char *av[])
 {
 	size_t sz;
 	FILE *fp;
+
+	PLTDEBUG(7,255,0,0);
+	RS232C_INIT();
 
 	if(2<=ac)
 	{
@@ -42,20 +48,39 @@ int main(int ac,char *av[])
 			fread(snd,1,sz,fp);
 			fclose(fp);
 
+			PLTDEBUG(7,0,255,0);
+			RS232C_PUTS("SND_init\r\n");
+
 			SND_init(SND_work);
+
+			PLTDEBUG(7,255,255,0);
+			RS232C_PUTS("SND_elevol_mute\r\n");
 
 			SND_elevol_mute(0x03); // WTF!  It says mute.  But, 1 means unmute.  Active-Low.  No mention in the red book.
 
+			PLTDEBUG(7,0,0,255);
+			RS232C_PUTS("SND_pcm_mode_set\r\n");
+
 			err=SND_pcm_mode_set(1);
 			printf("%d\n",err);
+
+			PLTDEBUG(7,255,0,255);
+			RS232C_PUTS("SND_pcm_play2\r\n");
+
 			err=SND_pcm_play2(71,snd[28],127,snd);
 			printf("%d\n",err);
 
 			while(SND_pcm_status(71)!=0);
 
+			RS232C_PUTS("SND_end\r\n");
 			SND_end();
 
+			PLTDEBUG(7,255,255,255);
+			RS232C_PUTS("free\r\n");
+
 			free(snd);
+
+			RS232C_PUTS("exit.\r\n");
 		}
 		else
 		{
