6063b2ce265e3ec4c1efc959c6e426ef53fb5929
Soji Yamakawa <soji@andrew.cmu.edu>
2024-11-29 11:45:47 -0500

    House keeping.

---
+++ tests/diskbios/diskbios.h
@@ -0,0 +1,16 @@
+#ifndef DISKBIOS_H_IS_INCLUDED
+#define DISKBIOS_H_IS_INCLUDED
+
+#define BIOS_FD0 0x20
+#define INT_DISKBIOS 0x93
+
+int DKB_restore(int devno);
+int DKB_rdstatus(int devno,unsigned int *status);
+int DKB_rdstatus2(int devno,unsigned int *mode,long *numSect);
+int DKB_seek(int devno,int C);
+int DKB_read(int devno,int C,int H,int R,int numSect,char *buf,int *actualReadCount);
+int DKB_read2(int devno,long LBA,int numSect,char *buf,int *actualReadCount);
+int DKB_write(int devno,int C,int H,int R,int numSect,char *buf,int *actualWriteCount);
+int DKB_write2(int devno,long LBA,int numSect,char *buf,int *actualWriteCount);
+
+#endif
