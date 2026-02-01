33ec5c595583f2654b15cc1a5a2db5690f8f8ec3
Soji Yamakawa <soji@andrew.cmu.edu>
2026-02-01 11:07:19 -0500

    SELGMDEV.EXP reduced from 24040 bytes to 11402 bytes by avoiding printf.

--- tgutil/SELGMDEV.C
+++ tgutil/SELGMDEV.C
@@ -16,15 +16,15 @@ int main(int ac,char *av[])
 
 	if(3!=ac)
 	{
-		printf("Usage: FREE386 SELGMDEV.EXP port_number game_dev_type\n");
-		printf("  Game device types:\n");
-		printf("    0...Normal or TOWNS Game Pad\n");
-		printf("    1...Fujitsu 6-button pad\n");
-		printf("    2...CAPCOM CPSF\n");
-		printf("    3...SHARP Cyber Stick\n");
-		printf("This program enables an expanded functionality of TGBIOS.\n");
-		printf("It let SND_joy_in_2 read from non-standard game pad and\n");
-		printf("interprets buttons 5 and 6 as run and select buttons.\n");
+		puts("Usage: FREE386 SELGMDEV.EXP port_number game_dev_type\n");
+		puts("  Game device types:\n");
+		puts("    0...Normal or TOWNS Game Pad\n");
+		puts("    1...Fujitsu 6-button pad\n");
+		puts("    2...CAPCOM CPSF\n");
+		puts("    3...SHARP Cyber Stick\n");
+		puts("This program enables an expanded functionality of TGBIOS.\n");
+		puts("It let SND_joy_in_2 read from non-standard game pad and\n");
+		puts("interprets buttons 5 and 6 as run and select buttons.\n");
 		return 1;
 	}
 
@@ -33,15 +33,19 @@ int main(int ac,char *av[])
 
 	if(1<port)
 	{
-		printf("Error: Port number needs to be 0 or 1.\n");
+		puts("Error: Port number needs to be 0 or 1.\n");
 		return 1;
 	}
 	if(3<type)
 	{
-		printf("Error: Type needs to be 0,1,2, or 3.\n");
+		puts("Error: Type needs to be 0,1,2, or 3.\n");
 	}
 
 	SND_SetGlobal_Gamedev_Type(port,type);
-	printf("Configured Game Device Type of Port %d to %s\n",port,devLabel[type]);
+	puts("Configured Game Device Type of Port ");
+	putc('0'+port,stdout);
+	puts(" to ");
+	puts(devLabel[type]);
+	puts("\n");
 	return 0;
 }
